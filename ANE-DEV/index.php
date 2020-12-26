a56df76af4dab857caa90aac752a8121b61d20a2
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2020-12-25 18:32:28 -0700

    More Images and added skeleton to php
    Php configuration for themes

--- ANE-DEV/index.php
+++ ANE-DEV/index.php
@@ -1,3 +1,24 @@
 <?php get_header(); ?>
-<h2>Hi from ayoola nurudeen etiko</h2>
+<?php
+
+// customized query
+// this helps display the posts you can also modify to get your projects and undergrad
+$args = array(
+    // associateve array
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+);
+$blogposts = new WP_Query($args);
+
+while ($blogposts->have_posts()) {
+    $blogposts->the_post();
+?>
+<!-- permalink helps to move to the page required -->
+    <a href="<?php the_permalink(); ?>">
+        <h3><?php the_title(); ?> </h3>
+    </a>
+    <?php the_excerpt(); ?>
+<?php
+}
+?>
 <?php get_footer(); ?>
